7dfb2286d28b9ee1d85a8e8f3e0889aee09127f1
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-02 00:07:45 +0300

    Intro-to-js-2-looping-code along

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Create a function that returns thank you message
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+    
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+// create a function for counts down from 10 to 0 
+
+function countDown(number){
+    while (number >=0) {
+        console.log(number);
+        number--;
+    }
+}
